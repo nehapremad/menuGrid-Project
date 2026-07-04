8bb01c22afec85941b58e641af73f64a8caa24fe
neha premad <nehaapremad@gmail.com>
2026-07-04 18:42:10 +0530

    bottom tab bar

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-};
+  plugins: [
+    '@babel/plugin-transform-export-namespace-from',
+    'react-native-worklets/plugin'
+
+  ],  
+}
